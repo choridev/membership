514bd1b1e5f44da69e37be6c6be1a3c8979412da
choridev <yidodeveloper@gmail.com>
2025-04-22 18:44:01 +0900

    ✏️ Fix user id header

--- springboot/src/main/java/com/example/springboot/util/MembershipConstants.java
+++ springboot/src/main/java/com/example/springboot/util/MembershipConstants.java
@@ -5,5 +5,5 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class MembershipConstants {
-    public static final String USER_ID_HEADER = "X-USER_ID";
+    public static final String USER_ID_HEADER = "X-USER-ID";
 }
